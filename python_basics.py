8fe89ac67d42abe390ee92dd175035edd33e6e11
langnalu19-ia <bernardyla@gmail.com>
2025-12-30 12:26:39 -0600

    Add files via upload

---
+++ python_basics.py
@@ -0,0 +1,33 @@
+age = 45
+height = 1.75
+
+name = "Bernard"
+message = "AI is powerful"
+
+print(age)
+print(height)
+print(name)
+print(message)
+
+songs = ["Praise", "Worship", "Adoration", "Thanksgiving"]
+print(songs)
+print(songs[0]) # First item
+print(songs[-1]) # last item
+
+songs.append("Joy")
+print(songs)
+
+song = {
+    "title": "Joy in the Middle of Trouble",
+    "tempo": 72,
+    "mood": "hopeful"
+}
+
+print(song)
+print(song["title"])
+print(song["mood"])
+
+if song["tempo"] > 70:
+    print("This song feels energetic")
+else:
+    print("This song feels calm")
